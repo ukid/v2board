dfe03a68778af812e4a3929bfeac8bbef8fa7512
bear <bear@beardeMac-mini.local>
2024-01-16 22:08:19 +0800

    修改特征

---
+++ public/theme/myboard/assets/env.example.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: 'MyBoard',
+  // 站点描述
+  description: 'MyBoard is best',
+  // API
+  host: '',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
